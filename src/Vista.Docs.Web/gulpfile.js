2e3f8c6e322afb84b4d642497dbc16b6eb616bca
AUCKLAND\RobertHu <robert.hughes@vista.co>
2017-10-07 12:26:29 +1300

    Added gulp build

--- src/Vista.Docs.Web/gulpfile.js
+++ src/Vista.Docs.Web/gulpfile.js
@@ -2,18 +2,16 @@
 var basePaths = {
     src: {
         base: './',
-        views: './Views',
-        images: './Images',
-        scripts: './Scripts',
-        styles: './Styles',
-        swagger: './SwaggerSrc'
+        images: './images',
+        scripts: './scripts',
+        styles: './css',
+        swagger: './swagger-src'
     },
     dest: {
-        base: '../WSVistaWebClient.Host/ApiDocsApp',
-        views: '../WSVistaWebClient.Host/ApiDocsApp/Views',
-        images: '../WSVistaWebClient.Host/ApiDocsApp/Images',
-        scripts: '../WSVistaWebClient.Host/ApiDocsApp/Scripts',
-        styles: '../WSVistaWebClient.Host/ApiDocsApp/Styles'
+        base: './assets',
+        images: './assets/images',
+        scripts: './assets/scripts',
+        styles: './assets/css'
     }
 };
 
@@ -30,12 +28,7 @@ var minifyCSS = require('gulp-csso');
 var concat = require('gulp-concat');
 var shell = require('gulp-shell');
 var print = require('gulp-print');
-var tfsCheckout = require('gulp-tfs-checkout');
 var runSequence = require('run-sequence');
-
-gulp.task('cshtml', function () {
-    return gulp.src([basePaths.src.views + '/**/*.cshtml', basePaths.src.views + '/**/*.config']).pipe(gulp.dest(basePaths.dest.views));
-});
 
 gulp.task('images', function () {
     return gulp.src([basePaths.src.images + '/**/*.*']).pipe(gulp.dest(basePaths.dest.images));
@@ -49,7 +42,7 @@ gulp.task('css', function () {
 
 gulp.task('scripts', function () {
     return gulp.src(scriptLibs)
-        .pipe(concat('Base.js'))
+        .pipe(concat('base.js'))
         .pipe(gulp.dest(basePaths.dest.scripts));
 });
 
@@ -61,25 +54,10 @@ gulp.task('swagger-css', function () {
     return gulp.src(basePaths.src.swagger + '/**/*.css')
         .pipe(gulp.dest(basePaths.dest.styles));
 });
-gulp.task('tfs-clean', function () {
-    return gulp.src(basePaths.dest.base + "/**/*.*")
-        .pipe(tfsCheckout());
-});
 
 
-gulp.task('unset-readonly', function () {
-    return gulp.src(basePaths.dest.base + "/**/*.*")
-        .pipe(print())
-        .pipe(shell("attrib -r <%= file.path %> /s"));
-});
-
-gulp.task('tfs', function (done) {
-    runSequence('tfs-clean','unset-readonly', done);
-});
 gulp.task('deploy', function (done) {
-    runSequence('unset-readonly','css', 'scripts', 'swagger-css', 'swagger-scripts', 'cshtml', 'images', done);
+    runSequence('css', 'scripts', 'swagger-css', 'swagger-scripts', 'images', done);
 });
 
-
-gulp.task('checkout', ['tfs']);
 gulp.task('default', ['deploy']);
